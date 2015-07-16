2d5d0d81e0d149d4b2d9c7895b55f8cd715c247a
David Pyke Le Brun <shoelace_822695@hotmail.com>
2015-07-16 16:48:57 +0100

    switch to dependent controller

--- directive.js
+++ directive.js
@@ -6,16 +6,36 @@
  */
 
 
-angular.module('shoelace.themeSwitcher',
-[])
-.controller('slThemeSwitcherCtrl',
-    ['$scope','$http',
+angular.module("shoelace.themeSwitcher",[])  
+.directive('slthemeswitcher', function () {
+
+    
+    var controller = ['$scope','$http', 
     function($scope,$http){
-        
+        /*
+        // initialize localStorage with default theme
+        $scope.$storage = $localStorage.$default({
+            theme: {   name: "", url }
+        });        
+        */
         $scope.activateTheme = function(theme)
         {
             //replace with switch on which version
-            $scope.currentThemeUrl = theme.cssCdn;
+            // don't do anything if the theme is the same
+            /*
+            if (theme.name !== $scope.themename) {
+               
+                // set the model so the directive updates
+                $scope.themename = theme;
+                $scope.currentThemeUrl = theme.cssCdn;
+                // save the new theme to localStorage
+                $scope.$storage.themename = theme.name;
+            } 
+            */
+           if ($scope.currentThemeUrl !== theme.cssCdn){
+                console.log('update theme');
+             $scope.currentThemeUrl = theme.cssCdn;
+         }
         }
 
         $http.get("http://api.bootswatch.com/3/")
@@ -25,21 +45,24 @@ angular.module('shoelace.themeSwitcher',
                  console.log("bootswatchversion:"+data.version);
      
 
-}).error(function(){
+}).error(function(error){
     //$(".alert").toggleClass("alert-info alert-danger");
     //$(".alert h4").text("Failure!");
-    console.log("ERROR");
+    console.log("ERROR:"+error);
 });
 
-    }])    
-.directive('slThemeSwitcher', function ThemeSwitcherDirective() {
-		return {
-			restrict: 'E',
-			scope: {
-		},
-			template: '<div ng-controller="slThemeSwitcherCtrl">{{currentTheme}}<link rel="stylesheet" ng-href="{{currentThemeUrl}}" /><select ng-model="currentTheme" required=true ng-options="theme.name for theme in bootswatch.themes" ng-change="activateTheme(currentTheme)"></select></div>'
-		};
-	})
+    }];
 
+
+   var template = '<link rel="stylesheet" ng-href="{{currentThemeUrl}}" /><select ng-model="currentTheme" required=true ng-options="theme.name for theme in bootswatch.themes" ng-change="activateTheme(currentTheme)"></select>';
+
+    return {
+        restrict: 'E',
+        controller: controller,
+        template: template
+        // or
+        // templateUrl: 'directive.html'
+    };
+})
 ;
 
